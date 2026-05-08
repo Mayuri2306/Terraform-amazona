7f0751ce7285c64d019ef15a6ec57e5c38da37b1
Mayuri2306 <mayurijhade104@gmail.com>
2026-05-08 19:33:53 +0530

    oac id in string resolved

--- Frontend/cloudfront.tf
+++ Frontend/cloudfront.tf
@@ -12,7 +12,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
     origin_id   = "s3-origin"
-    origin_access_control_id = "aws_cloudfront_origin_access_control.s3_oac.id"
+    origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
   }
 
   default_cache_behavior {
