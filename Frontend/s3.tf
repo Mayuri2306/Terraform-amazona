5c75eaea7b09b2fe8341c5e3b9697437ab45b8ba
Mayuri2306 <mayurijhade104@gmail.com>
2026-05-08 20:03:06 +0530

    removed condition block s3

--- Frontend/s3.tf
+++ Frontend/s3.tf
@@ -28,11 +28,7 @@ resource "aws_s3_bucket_policy" "allow_read_access" {
       },
       "Action": "s3:GetObject",
       "Resource": "${aws_s3_bucket.frontend_bucket.arn}/*",
-      "Condition": {
-        "StringEquals": {
-          "AWS:SourceArn": "${aws_cloudfront_distribution.s3_distribution.arn}"
-        }
-      }
+      
     }
   ]
 })
